7a29a590ea22652e46dd69c5e356bb140384d329
Erin McCarthy <mccarthyern@gmail.com>
2017-11-24 17:22:07 -0800

    added some yummy global variables

--- project/bayesnet.py
+++ project/bayesnet.py
@@ -5,7 +5,17 @@ from divLabels import learn
 # TODO
 #this should probably take the log instead of floating point multi
 #becasue the numbers can get too small
-def train:
+
+#my ugly global variables
+trainPositive = {}
+trainNegative = {}
+negativeTotal = 0
+positiveTotal = 0
+pA = 0
+pNotA = 0
+
+def train():
+    global pA, pNotA
     total = 0
     numSpam = 0
     for (label, fileName) in learn:
@@ -13,12 +23,14 @@ def train:
             numSpam +=1
         total += 1
         processEmail(fileName, label)
-    pA = numSpam/(float)total
-    pNotA = (total - numSpam)/(float)total
+    pA = numSpam / float(total)
+    pNotA = (total - numSpam) / float(total)
 
 def processEmail(fileName, label):
+    global trainPositive
+    global trainNegative, positiveTotal, negativeTotal
     path = "/mnt/c/Users/merin/Desktop/CIS571/CSDMC2010_SPAM/CSDMC2010_SPAM/CLEANED_TRAIN"
-    fi = open(os.join.path(path, fileName), "r")
+    fi = open(os.path.join(path, fileName), "r")
     for line in fi:
         for word in line.split():
             if label == 0:
@@ -29,6 +41,8 @@ def processEmail(fileName, label):
                 negativeTotal +=1
 
 def conditionalWord(word, label):
+    global trainPositive
+    global trainNegative, positiveTotal, negativeTotal
     if label == 0:
         return (trainPositive.get(word, 0)+1)/(float)(positiveTotal+(1*numWords))
     return (trainNegative.get(word,0)+1)/(float)(negativeTotal+(1*numWords))
@@ -43,9 +57,10 @@ def conditionalEmail(fileName, label):
     return result
 
 def classify(fileName):
+    global pA, pNotA
     isSpam = pA * conditionalEmail(fileName, 0)
     notSpam = pNotA * conditionalEmail(fileName, 1)
     return isSpam > notSpam
                 
-            
+train()            
         
